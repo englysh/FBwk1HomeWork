1d4e4c80fead6d15ca8e021cf813421ba7be4d5d
Engly Chang <engly@strongbetter-lm.att.net>
2014-06-09 15:10:06 -0700

    Initial Commit

---
+++ FBwk1HomeWork/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FBwk1HomeWork
+//
+//  Created by Engly Chang on 6/9/14.
+//  Copyright (c) 2014 Engly Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FBwk1HomeWork/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FBwk1HomeWork
+//
+//  Created by Engly Chang on 6/9/14.
+//  Copyright (c) 2014 Engly Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
